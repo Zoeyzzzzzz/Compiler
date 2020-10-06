17fc01728ac27a8cf0a42944d79224c7b6f1c1b1
Zoeyzzzzzz <52778162+Zoeyzzzzzz@users.noreply.github.com>
2020-10-06 11:38:06 +0800

    Update word.c

--- word.c
+++ word.c
@@ -3,131 +3,201 @@
 #include<stdlib.h>
 #include<ctype.h>
 
-char c;
-char token[500], line[1001];
-//now代表现在进行的字符位置
-//isContinue代表是否继续扫描
-//token_now代表当前单词字符串的末位置
-int now = 0, isContinue=0, token_now=0;
+char txt[1000];
+int now;
+ 
+char token[300];
+int array;
 
-void getChar(){
-	c = line[now++];
-}
+int num;
 
-void cat(){
-	token[token_now++] = c;
-}
+char recent; 
+
+int type;
+
+int go;
+
+
+void clearToken(){
+	memset(token,'\0',sizeof(token));
+	array=0;
+	recent='\0';
+	type=0;
+} 
+
+
+void getchar1(){
+	recent=txt[now];
+	now++; 
+} 
+
+void retract(){
+	now--;
+} 
+
+
+void catToken(){
+	token[array]=recent;
+	array++;
+} 
+
+
+int isSpace(){
+	if(recent==' '||recent=='\t'||recent=='\n'||recent=='\r'){
+		return 1;
+	}
+	else return 0;
+} 
+
 
 int isLetter(){
-	if(isupper(c)||islower(c)) return 1;
-	else return 0;
-}
+	if(isupper(recent)||islower(recent)) return 1;
+	return 0;
+} 
 
 int isDigit(){
-	if(isdigit(c)) return 1;
+	if(isdigit(recent)) return 1;
+	return 0;
+} 
+
+
+int reserver(){
+	if(!strcmp(token,"BEGIN")) return 1;
+	else if(!strcmp(token,"END")) return 2;
+	else if(!strcmp(token,"FOR")) return 3;
+	else if(!strcmp(token,"IF")) return 4;
+	else if(!strcmp(token,"THEN")) return 5;
+	else if(!strcmp(token,"ELSE")) return 6;
 	else return 0;
-}
+} 
 
-void unGetch(){
-	now--;
-}
 
-void reserve(){
-	//非0是保留字和类别
-	//0是标识符
-	if(strcmp(token, "BEGIN") == 0) printf("Begin\n");
-	else if(strcmp(token, "END") == 0) printf("End\n");
-	else if(strcmp(token, "FOR") == 0) printf("For\n");
-	else if(strcmp(token, "DO") == 0) printf("Do\n");
-	else if(strcmp(token, "IF") == 0) printf("If\n");
-	else if(strcmp(token, "THEN") == 0) printf("Then\n");
-	else if(strcmp(token, "ELSE") == 0) printf("Else\n");
-	else printf("Ident(%s)\n", token);
-}
-
-int atoiNew(){
-	int num = 0;
-	for(int i=0; i<token_now; i++){
-		num *= 10;
-		num += token[token_now] - '0';
+int transNum(){
+	int number=0;
+	int i=0;
+	for(i=0;i<array;i++){
+		number*=10;
+		number=number+token[i]-'0';
 	}
-	return num;
-}
+	return number;
+} 
+
 
 void error(){
-	isContinue = 1;
 	printf("Unknown\n");
+	go=1;
 }
 
+void chu(){
+	switch(type){
+		case 1:
+			printf("Begin\n");
+			break;
+		case 2:
+			printf("End\n");
+			break;
+		case 3:
+			printf("For\n");
+			break;
+		case 4:
+			printf("If\n");
+			break;
+		case 5:
+			printf("Then\n");
+			break;
+		case 6:
+			printf("Else\n");
+			break;	
+		case 7:
+			printf("Ident(%s)\n",token);
+			break;	
+		case 8:
+			printf("Int(%d)\n",num);
+			break;	
+		case 9:
+			printf("Colon\n");
+			break;
+		case 10:
+			printf("Plus\n");
+			break;
+		case 11:
+			printf("Star\n");
+			break;
+		case 12:
+			printf("Comma\n");
+			break;
+		case 13:
+			printf("LParenthesis\n");
+			break;
+		case 14:
+			printf("RParenthesis\n");
+			break;
+		case 15:
+			printf("Assign\n");
+			break;
+		default:
+			break;	
+	}
+}
 
-void getNbc(){
-	while(line[now]!='\0' && isContinue==0){
-		memset(token,'\0',sizeof(token));
-		token_now = 0;
-		c = '\0';
-
-		getChar();
-		//如果是空白字符，继续读下一个
-    	while(c==' ' || c=='\t' || c=='\n' || c=='\r') getChar();
-
-    	//如果是字母
+void getsym(){
+	while(txt[now]!='\0'&&go==0){
+		clearToken();
+		getchar1();
+    	while(isSpace()) getchar1();
     	if(isLetter()){               
-    		while(isLetter() || isDigit()){
-	    		cat();
-		    	getChar();
+    		while(isLetter()||isDigit()){
+	    		catToken();
+		    	getchar1();
 	    	}
-	    	unGetch();
-	    	reserve(); 
-    	}
-
-    	//如果是数字
+	    	retract();
+	    	int end=reserver();  
+	    	if(end==0) {
+	    		type=7; 
+	    	}
+	    	else type=end; 
+    	} 
     	else if(isDigit()){
     		while(isDigit()){
-    			cat();
-    			getChar();
+    			catToken();
+    			getchar1();
     		}
-	    	unGetch();
-	    	printf("Int(%d)\n", atoiNew());
+	    	retract();
+	    	num=transNum();
+    		type=8;
     	}
-
-    	//如果是单字符分界符
-    	else if(c == '+') printf("Plus\n");
-    	else if(c == '*') printf("Star\n");
-    	else if(c == ',') printf("Comma\n");
-    	else if(c == '(') printf("LParenthesis\n");
-    	else if(c == ')') printf("RParenthesis\n");
-
-    	//如果是冒号状态，分下一个是等号还是非等号
-    	else if(c == ':'){
-    		//继续读下一个
-    		getChar();
-    		//如果是等号
-    		if(c == '=') printf("Assign\n");
-    		//如果不是等号
+    	else if(recent==':'){
+    		getchar1();
+    		if(recent=='=') type=15; 
     		else{
-	           printf("Colon\n");
-	           unGetch();
+	           type=9; 	
+	           retract();
     		} 
 		
     	}
-     	
-    	else if(c=='\0') break;
+     	else if(recent=='+') type=10;
+    	else if(recent=='*') type=11;
+    	else if(recent==',') type=12;
+    	else if(recent=='(') type=13;
+    	else if(recent==')') type=14;
+    	else if(recent=='\0') break;
       	else error();
+      	
+    	chu();
 	}
+
 }
-
-
+ 
 
 int main(int argc, char *argv[]){
+
 	FILE *fp = fopen(argv[1],"r");
-	if(fp == NULL) printf("error\n"); 
-
-	while(fgets(line,1000,fp)!=NULL && isContinue==0){
-		now = 0;
-		getNbc();
-		memset(line,'\0',sizeof(line));
+	if(fp==NULL) printf("error"); 
+	while(fgets(txt,997,fp)!=NULL&&go==0){
+		now=0;
+		getsym();
+		memset(txt,'\0',sizeof(txt));
 	}
-
 	fclose(fp);
 	return 0;
 }
